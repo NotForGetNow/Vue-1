371e9eaedde44a451c3a264c737b11b19fceb874
Ануар Канат Ануарулы <171016428@step.kz>
2024-07-11 20:10:50 +0500

    search

--- src/App.vue
+++ src/App.vue
@@ -42,8 +42,6 @@ const fetchitems = async () => {
   }
 }
 
-
-
 const onChangeSelect = (event) => {
   filters.sortBy = event.target.value
 }
@@ -66,32 +64,7 @@ watch(filters, fetchitems)
       <Adidas />
     </div>
 
-    <div class="flex justify-between pr-10">
-      <h2 class="font-bold text-xl p-10">Все кроссовки</h2>
-      <div class="flex gap-3 items-center">
-        <select
-          @change="onChangeSelect"
-          class="border border-gray-200 rounded-md outline-none py-2 px-2"
-        >
-          <option value="title">По названию</option>
-          <option value="price">По возрастанию цены</option>
-          <option value="-price">По убыванию цены</option>
-        </select>
-        <div class="relative">
-          <input
-            @input="onChangeInput"
-            type="text"
-            class="border border-gray-200 rounded-md py-2 pl-10 pr-4 focus:outline-none focus:border-gray-400"
-            placeholder="Поиск..."
-          />
-          <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-            <img src="/search.svg" />
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <Search /> 
+    <Search :onChangeSelect="onChangeSelect" :onChangeInput="onChangeInput" /> 
 
     <Cards :items="items" />
   </div>
